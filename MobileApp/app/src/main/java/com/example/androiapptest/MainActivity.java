cebcc103d9350e1fd0fd3468c5f62fa9bb6cf197
Eduardo Montenegro <eduardo-thrash@hotmail.com>
2020-05-30 22:12:35 -0500

    First test with Login layout

--- MobileApp/app/src/main/java/com/example/androiapptest/MainActivity.java
+++ MobileApp/app/src/main/java/com/example/androiapptest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androiapptest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        Intent Login = new Intent(this, LoginActivity.class);
+        startActivity(Login);
+
+        //setContentView(R.layout.activity_main);
     }
 }
